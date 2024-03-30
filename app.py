35e90983e7dc1e550b35fd29145ec62b2c6c0948
Bharadwaj Murthy <bharadwajmurthy@Bharadwajs-MacBook-Pro.local>
2024-03-30 12:06:32 +0100

    basic_UI_part

---
+++ app.py
@@ -0,0 +1,23 @@
+
+import streamlit as st
+from dotenv import load_dotenv
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with Multiple pdfs",page_icon=":books:")
+    
+    st.header("Chat with Multiple pdfs")
+    
+    st.text_input("Ask a question about your documents")
+    
+    with st.sidebar:
+        st.subheader("Your documents")
+        
+        st.file_uploader("Upload your pdfs here and click on 'Process'")
+        
+        st.button("Process")
+
+
+
+if __name__ == '__main__':
+    main()
