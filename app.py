8ce4d818540e43a249f968799140b083b4bbb964
Bharadwaj Murthy <bharadwajmurthy@Bharadwajs-MacBook-Pro.local>
2024-03-30 13:36:54 +0100

    get-text-chunks-method-implemented

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 
 import streamlit as st
 from dotenv import load_dotenv
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 def get_pdf_text(pdf_docs):
     text=""
     for pdf in pdf_docs:
-        pdf_reader=PdfFileReader(pdf)
+        pdf_reader=PdfReader(pdf)
         for page in pdf_reader.pages:
             text+=page.extract_text()
     return text
+
+def get_text_chunks(text):
+    text_splitter=CharacterTextSplitter()
+    text_chunks= text_splitter.split_text(text)
+    return text_chunks
 
 def main():
     load_dotenv()
@@ -31,7 +37,8 @@ def main():
                 raw_text = get_pdf_text(pdf_docs)
                 
                 #get the text chunks
-                
+                text_chunks = get_text_chunks(raw_text)
+                st.write(raw_text)
                 #create vector store
 
 
